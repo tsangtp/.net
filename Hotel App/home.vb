f7d1b3b550771994d7e8d85d3e4db8a79551552c
tsangtp <66738923+tsangtp@users.noreply.github.com>
2022-05-12 12:32:47 +0800

    Add files via upload

---
+++ Hotel App/home.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Data.SqlClient
+Public Class home
+    Dim p1 As Integer = 40
+    Private Sub home_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblname.Text = login.txtname.Text
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If p1 > 80 Then
+            Timer1.Stop()
+        Else
+            Me.pn.Size = New Size(Me.pn.Size.Width, p1)
+            p1 += 10
+        End If
+    End Sub
+
+    Private Sub lblname_MouseHover(sender As Object, e As EventArgs) Handles lblname.MouseHover
+        Timer1.Start()
+    End Sub
+
+    Private Sub lblname_MouseLeave(sender As Object, e As EventArgs) Handles lblname.MouseLeave
+        Timer1.Stop()
+        p1 = 40
+    End Sub
+
+
+    Private Sub lblsetting_Click(sender As Object, e As EventArgs)
+        Me.Close()
+    End Sub
+
+    Private Sub lbllogout_Click(sender As Object, e As EventArgs) Handles lbllogout.Click
+        Me.Close()
+        login.Show()
+    End Sub
+
+
+    Private Sub btnpersonal_Click(sender As Object, e As EventArgs) Handles btnpersonal.Click
+        updates.Show()
+    End Sub
+
+    Private Sub btnbooking_Click(sender As Object, e As EventArgs) Handles btnbooking.Click
+        booking.Show()
+    End Sub
+
+    Private Sub btnrecord_Click(sender As Object, e As EventArgs) Handles btnrecord.Click
+        record.Show()
+    End Sub
+End Class
---
+++ Hotel App/login.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Data.SqlClient
+
+
+Public Class login
+    Private Sub btnlogin_Click(sender As Object, e As EventArgs) Handles btnlogin.Click
+        Dim con As SqlConnection = New SqlConnection("server=.;uid=sa;pwd=sa;database=project1;")
+        Try
+            con.Open()
+            Dim cmd As SqlCommand = New SqlCommand($"select count(*) from dbo.login where username = N'{txtname.Text}' and password = N'{txtpwd.Text}'", con)
+
+            If cmd.ExecuteScalar() > 0 Then
+                Me.Hide()
+                home.Show()
+
+            Else
+                MsgBox("Input error!Please enter again.")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        con.Close()
+    End Sub
+
+    Private Sub lblregister_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lblregister.LinkClicked
+        register.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub lblforgetpwd_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lblforgetpwd.LinkClicked
+        findpassowrd.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Refresh()
+        txtname.Text = ""
+        txtpwd.Text = ""
+    End Sub
+
+
+End Class
